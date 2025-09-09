33d5a4c0833a384976d989cdc7a47f8da4e39a44
Roee Eidan <roeeeidan@Roees-MacBook-Pro.local>
2025-09-09 15:09:08 +0300

    Initial commit

---
+++ test/chain-prices.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ChainPrices from '../lib/chain-prices-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/chain-prices-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ChainPrices.ChainPricesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
